f46e9f1ba8cfd34c26c093714e1bd44829a8145f
Anatoliy Katelnitskiy <katel1998@yandex.ru>
2023-01-21 16:52:09 +0300

    add Readme, fix test

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# ajs-task-12.1-by-kiraradi-
+# [Домашнее задание к лекции «TypeScript»](https://github.com/netology-code/ajs-homeworks/tree/ajs8/typescript)
+
+## Новые типы
+
+[Мое портфолио](https://kiraradi.github.io/Portfolio/)
+
+## Мои социальные сети:
+-[VK](https://vk.com/thedomino2)
+
+-[Telegram](https://t.me/Kiraradi)
--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -2,6 +2,19 @@ import Movie from "../Movie";
 
 test('test 1', () => { 
     const desuNoto = new Movie('Тутрадь Смерти', 'Desu noto', 2006, 'Japan', 'Take down Yotsuba!', ['anome','fantasy', 'thriller', 'drama', 'crime', 'detective'],814, 'https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/0368a21d-64dd-4340-b097-205dc9512ba4/300x450',1,1200,'Movie');
-    console.log(desuNoto);
-    expect(1).toBe(1);
+    //console.log(desuNoto);
+    const rightAnswer = {
+        localizedName: 'Тутрадь Смерти',
+        originalName: 'Desu noto',
+        year: 2006,
+        country: 'Japan',
+        slogan: 'Take down Yotsuba!',
+        genre: [ 'anome', 'fantasy', 'thriller', 'drama', 'crime', 'detective' ],
+        timeInMinutes: 814,
+        poster: 'https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/0368a21d-64dd-4340-b097-205dc9512ba4/300x450',
+        id: 1,
+        price: 1200,
+        category: 'Movie'
+      }
+    expect(desuNoto).toEqual(rightAnswer);
  })
